3290d370c265bad853c08a87762feeb67ccad637
Isaac <iaddai2003@gmail.com>
2023-10-08 20:07:47 -0400

    set up boilerplate code for fullpage

---
+++ script.js
@@ -0,0 +1,8 @@
+new fullpage('#fullpage', {
+    //options here
+    autoScrolling:true,
+    scrollHorizontally: true,
+    sectionsColor: ['#fe5f46', '#0998ec', '#fc6c7c', '#fec304'],
+    
+});
+
